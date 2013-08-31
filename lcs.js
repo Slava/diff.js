b33356f976b62a78c3797cfd6e3138cbc0124e1f
Slava Kim <slava@meteor.com>
2013-08-31 15:35:28 -0700

    Define internal function midPoint

--- lcs.js
+++ lcs.js
@@ -5,5 +5,13 @@
 // @param equals - optional comparator of atoms - returns true or false,
 //                 if not speicified, tripple equals operator is used
 // @returns sequence of atoms, one of LCSs
-var LCS = function (A, B, /* optional */ equals) {};
+var LCS = function (A, B, /* optional */ equals) {
+  // Get the the point in Edit Graph, one of the LCS pathes goes through.
+  // Ideally the point equally diveds [startA;endA] and [startB;endB] in two
+  // parts each.
+  // @param startA, endA - substring of A we are working on
+  // @param startB, endB - substring of B we are working on
+  // @returns Array - [midA, midB] - midpoints
+  var midPoint = function (startA, endA, startB, endB) {};
+};
 
