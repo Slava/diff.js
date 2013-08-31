6d46d0eace4b9988701540408329415a303e9936
Slava Kim <slava@meteor.com>
2013-08-31 15:21:21 -0700

    Init commit. Define the API.

---
+++ lcs.js
@@ -0,0 +1,9 @@
+
+// Longest Common Subsequence
+// @param A - sequence of atoms - String or Array
+// @param B - sequence of atoms - String or Array
+// @param equals - optional comparator of atoms - returns true or false,
+//                 if not speicified, tripple equals operator is used
+// @returns sequence of atoms, one of LCSs
+var LCS = function (A, B, /* optional */ equals) {};
+
